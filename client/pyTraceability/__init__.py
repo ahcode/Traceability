f7bb28d843d064025b0bd5a242223991e3ba921a
ahcode <alvaro.hpg4@gmail.com>
2018-08-06 17:41:38 +0200

    send functions

--- client/pyTraceability/__init__.py
+++ client/pyTraceability/__init__.py
@@ -4,18 +4,18 @@ import json
 from pyTraceability.keys import Key
 from pyTraceability.api_interface import send_transaction
 
+#TRANSACTION MODES
+# 0 - MERGE
+# 1 - QUEUE
+# 2 - STACK
+# 3 - ARBITRARY (PRODUCT ID REQUIRED)
+
 class Connection():
-    def __init__(self, keyfile, mode):
+    def __init__(self, keyfile):
         self.__apicheck()
 
         if not isinstance(keyfile, str): raise Exception("keyfile must be a string")
         self.key = Key(keyfile)
-
-        if mode == 'merge': self.mode = 0
-        elif mode == 'queue': self.mode = 1
-        elif mode == 'stack': self.mode = 2
-        elif mode == 'arbitrary': self.mode = 3
-        else: raise Exception("mode must be 'merge', 'serial' or 'arbitrary'")
 
     def __apicheck(self):
         return True
@@ -23,10 +23,13 @@ class Connection():
         #Conectar a la api y esperar una determinada respuesta
         #Si no se recibe lanzar excepción
 
-    def __newtransaction(self, transaction_type, receiver, data):
+    def __newtransaction(self, transaction_type, mode, receiver, data):
         transmitter = self.key.get_hash()
         ordered_data = OrderedDict(sorted(data.items()))
-        transaction = [("type", transaction_type), ("mode", self.mode), ("transmitter", transmitter), ("timestamp", int(time())), ("data", ordered_data)]
+        transaction = [("type", transaction_type), ("mode", mode), ("transmitter", transmitter)]
+        if(receiver):
+            transaction.append(("receiver", receiver))
+        transaction.extend([("timestamp", int(time())), ("data", ordered_data)])
         transaction = OrderedDict(transaction)
         serialized_transaction = json.dumps(transaction, separators = (',',':'))
         sign = self.key.get_sign(serialized_transaction)
@@ -34,12 +37,36 @@ class Connection():
         serialized_transaction = json.dumps(transaction, separators = (',',':'))
         send_transaction(serialized_transaction)
 
-
-    def generate(self, product, quantity, origin, *args, **kwargs):
+    def generate(self, product, quantity, origin, additional_data = {}):
         if not isinstance(product, str): raise Exception("product must be a string")
         if not isinstance(quantity, int): raise Exception("quantity must be an integer")
         if quantity <= 0: raise Exception("quantity must be greater than 0")
         if not isinstance(origin, str): raise Exception("origin must be a string")
+        if not isinstance(additional_data, dict): raise Exception("additional_data must be a dictionary")
 
-        data = {"product": product, "quantity": quantity, **kwargs}
-        self.__newtransaction(0, None, data)
+        data = {"product": product, "quantity": quantity, **additional_data}
+        self.__newtransaction(0, 0, None, data)
+    
+    def send(self, mode, receiver, product, quantity = None, additional_data = {}):
+        if not isinstance(mode, int): raise Exception("mode must be an integer")
+        if mode < 0 or mode > 2: raise Exception("mode must be between 0 and 2")
+        if not isinstance(receiver, str): raise Exception("product must be a string")
+        if not isinstance(product, str): raise Exception("product must be a string")
+        if quantity and not isinstance(quantity, int): raise Exception("quantity must be an integer or null")
+        if quantity and quantity <= 0: raise Exception("quantity must be greater than 0")
+        if not isinstance(additional_data, dict): raise Exception("additional_data must be a dictionary")
+        
+        data = {"product": product, **additional_data}
+        if quantity: data['quantity'] = quantity
+        
+        self.__newtransaction(2, mode, receiver, data)
+
+    def send_by_id(self, receiver, product, product_id, additional_data = {}):
+        if not isinstance(receiver, str): raise Exception("product must be a string")
+        if not isinstance(product, str): raise Exception("product must be a string")
+        if not isinstance(product_id, 'str'): raise Exception("product_id must be a string")
+        if not isinstance(additional_data, dict): raise Exception("additional_data must be a dictionary")
+
+        data = {"product": product, "product_id": product_id, **additional_data}
+
+        self.__newtransaction(2, 3, receiver, data)
