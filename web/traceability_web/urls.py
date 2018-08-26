8ad4c2153ad654acc497c7c2b40ed8deff463602
ahcode <alvaro.hpg4@gmail.com>
2018-08-26 19:03:58 +0200

    login y logout usuarios

--- web/traceability_web/urls.py
+++ web/traceability_web/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name = 'users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page = reverse_lazy('index')), name='logout'),
     path('', include('traceability.urls')),
 ]
